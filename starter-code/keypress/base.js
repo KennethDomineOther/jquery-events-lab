5055ccb3413899918272df7bf6cb88a4620c8690
Kenneth Domine <KenDomine@Kenneths-MacBook-Pro.local>
2017-09-28 23:19:26 -0700

    Can't get the stop watch to stop.

--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -1,5 +1,28 @@
 console.log("Sanity Check: JS is working!");
 
+
+var startTime; 
+var endTime;
+var total = 0;
+
 $(document).ready(function(){
+  $(window).on("keypress", function handleKeypress(event){
+
+    if ( startTime ) {
+      endTime = Date.now();
+      total = (endTime - startTime) / 1000;
+      displayTotal();
+    } else {
+      startTime = Date.now();
+
+      window.setInterval(displayTotal, 500);
+    }
+
+  })
 
 })
+
+function displayTotal(){
+  total = (Date.now() - startTime) / 1000;
+  $("#total-time").text( total + " seconds" );
+}
